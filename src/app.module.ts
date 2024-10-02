cce44433379843867d53c0657495536a4bdfddf8
Carlos Daniel Angel Castaño <cdangel13@gmail.com>
2024-10-02 10:34:19 -0500

    Add crud of user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { ClientsModule } from './clients/clients.module';
 import { CategoriesModule } from './categories/categories.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ClientsModule, CategoriesModule],
+  imports: [ClientsModule, CategoriesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,23 @@
+import { Role } from '@prisma/client';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  @IsNotEmpty()
+  password: string;
+
+  @IsEnum(Role)
+  @IsNotEmpty()
+  role: Role;
+}
