a75f466a8b73775eec1b3f68764fd96d421c7386
Carlos Daniel Angel Castaño <cdangel13@gmail.com>
2024-10-01 20:54:08 -0500

    add crud functions of prisma

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ClientsService {
+  constructor(private prismaService: PrismaService) {}
+
   create(createClientDto: CreateClientDto) {
-    return 'This action adds a new client';
+    return this.prismaService.client.create({
+      data: createClientDto,
+    });
   }
 
   findAll() {
-    return `This action returns all clients`;
+    return this.prismaService.client.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} client`;
+  findOne(id: string) {
+    return this.prismaService.client.findUnique({
+      where: { id },
+    });
   }
 
-  update(id: number, updateClientDto: UpdateClientDto) {
-    return `This action updates a #${id} client`;
+  update(id: string, updateClientDto: UpdateClientDto) {
+    return this.prismaService.client.update({
+      data: updateClientDto,
+      where: { id },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} client`;
+  remove(id: string) {
+    return this.prismaService.client.delete({
+      where: { id },
+    });
   }
 }
